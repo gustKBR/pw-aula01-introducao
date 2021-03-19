fc88878229ade57a1c634b72ebfb0072b0fd5a6e
gustKBR <gustavocabralcorreia@gmail.com>
2021-03-19 14:50:34 +0000

    Aula 01 - Primeiro componente

---
+++ fundamentos01/src/components/basico/OlaMundo.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default () => {
+    const informacao = 'Primeiro componente JSX'
+
+    return(
+        <>
+            <h2>Componente OlaMundo</h2>
+            <p>{ informacao }</p>
+        </>
+    )
+}
